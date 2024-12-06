cae5b1486188fc66f49aa37bb2467a91ec70b8ad
iKrazyCode <dodochavescadete13@gmail.com>
2024-12-06 19:09:22 -0300

    update

--- home/views.py
+++ home/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('oi')
+    return render(request, 'home/index.html')
+
+def monte(request):
+    return render(request, 'home/monte.html')
