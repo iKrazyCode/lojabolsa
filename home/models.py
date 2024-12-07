9b5a2ff9456ed61e39066fb4702beb2c75d6d9b8
iKrazyCode <dodochavescadete13@gmail.com>
2024-12-07 14:58:59 -0300

    update

--- home/models.py
+++ home/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Cores(models.Model):
+    """
+    Model responsável por armazenar as cores que terão disponíveis para escolha
+    """
+    ...
