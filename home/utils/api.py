be27120c5e2374c2c7108c2ac1745e2192727a57
iKrazyCode <dodochavescadete13@gmail.com>
2024-12-08 17:17:57 -0300

    up

--- home/utils/api.py
+++ home/utils/api.py
@@ -20,7 +20,7 @@ class NuvemShopApi:
             headers=NuvemShopApi.HEADERS,
             params=params
             )
-        r.raise_for_status()
+        #r.raise_for_status()
         return r.json()
 
 
@@ -31,6 +31,30 @@ class NuvemShopApi:
             f"{NuvemShopApi.BASE_URL}{endpoint}",
             headers=NuvemShopApi.HEADERS,
             json=data)
-        r.raise_for_status()
+        # r.raise_for_status()
         return r.json()
-     
+
+    # Helpers
+    @staticmethod
+    def get_products_id(products_urls=list) -> list:
+        """
+        Retorna uma lista com o [URL do produto, id do produto] de cada produto contido em products_url
+        """
+        list_id = []
+        for url in products_urls:
+            url_part = str(url)
+            if str(url_part).endswith('/'):
+                url_part = url_part[:-1].split('/')[-1]
+            else:
+                url_part = url_part.split('/')[-1]
+            
+            r = NuvemShopApi.get('/products/', params={'handle': url_part, 'fields': 'id'})
+            prod_id = r
+            list_id.append([url, str(prod_id[0]['id'])])
+        
+        return list_id
+    
+
+
+
+
--- home/views.py
+++ home/views.py
@@ -6,6 +6,24 @@ def home(request):
     return render(request, 'home/index.html')
 
 def monte(request):
-    cart = NuvemShopApi.post('/draft/')
-    
+    products_id = NuvemShopApi.get_products_id(products_urls=[
+         'https://enepta.lojavirtualnuvem.com.br/produtos/carro-importado/',
+         'https://enepta.lojavirtualnuvem.com.br/produtos/jaqueta-de-couro/'
+    ])
+
+    products_list_variants = []
+    for prod_url, prod_id in products_id:
+        products_list_variants.append({'variant_id': prod_id, 'quantity': 1})
+
+    data = {
+        'contact_name': 'teste',
+        'contact_lastname': 'testedg',
+        'contact_email': 'teste@gmail.com',
+        'payment_status': 'unpaid',
+        'products': products_list_variants,
+    }
+    # cart = NuvemShopApi.post('/draft_orders/', data=data)
+
+    # print(cart)
+
     return render(request, 'home/monte.html')
