4f9b059748f7b629aa5d82af5b629c18204d77a6
iKrazyCode <dodochavescadete13@gmail.com>
2024-12-12 12:44:22 -0300

    up

--- home/templatetags/custom_filters.py
+++ home/templatetags/custom_filters.py
@@ -8,19 +8,3 @@ def contains(palavra, substring):
 
 
 
-
-
-"""
-
-                            <div class="col">
-                                <div class="bg-white w-100">
-                                    <div class="box border-1 position-relative mx-auto" style="width: 300px;height: 300px;">
-                                        <img id="frentex" src="{% static 'home/img/reais/frente-preta.png' %}" alt=""
-                                            class="w-100 position-absolute top-0 start-0" style="z-index: 1;">
-                                        <img id="alcax" src="{% static 'home/img/reais/alca-preta.png' %}" alt=""
-                                            class="w-100 position-absolute top-0 start-0" style="z-index: 0;">
-                                    </div>
-                                </div>
-                            </div>
-
-"""
